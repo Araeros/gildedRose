de43e15b620aa1a43ef9e0f82f227b36c78cfc78
Romain Voquer <rvoquer@gmail.com>
2020-09-25 11:32:44 +0200

    ceci est un test

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+// ceci est un test
 class GildedRoseTest {
 
     @Test
