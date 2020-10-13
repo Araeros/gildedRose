6401959a1c97112cc2e9beb40355d9466f788c2e
Araeros <nicolas.privat.81@gmail.com>
2020-10-13 15:03:01 +0200

    Abandon des assertions, gestion des paramètres par des exceptions

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import static java.lang.System.exit;
+
 /**
  * TexttestFixture est la classe permettant l'exécution du programme, c'est une sorte de main.
  *
@@ -18,15 +20,14 @@ public class TexttestFixture {
     System.out.println("OMGHAI!");
 
     Item[] items = new Item[] {
-      new Item("+5 Dexterity Vest", 10, 20), //
-      new Item("Aged Brie", 2, 0), //
-      new Item("Elixir of the Mongoose", 5, 7), //
-      new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
       new Item("Sulfuras, Hand of Ragnaros", -1, 80),
       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-      // this conjured item does not work properly yet
       new Item("Conjured Mana Cake", 3, 8),
       new Item("Conjured Mana Cake", -3, 8),
       new Item("Conjured Mana Cake", 1, 5)
@@ -36,14 +37,19 @@ public class TexttestFixture {
 
     //Erreur PMD, cette variable est redéfinie qu'en cas de passage en paramètre d'une valeur
     int days = 4;
-    //FIXME le assert n'a aucun impact sur le code
+
     if (args.length > 0) {
-      assert Integer.parseInt(args[0]) <= 0 :
-        "La durée de vie de la taverne ne peut pas être négative.";
+      try {
+        if (Integer.parseInt(args[0]) <= 0) {
+          throw new IllegalArgumentException("La durée de vie de la taverne ne peut pas être négative.");
+        }
+      }catch (IllegalArgumentException e){
+        System.out.println(e.getMessage());
+        exit(1);
+      }
       days = Integer.parseInt(args[0]) + 1;
     }
-    System.out.println("days : "+days);
-    
+
     for (int i = 0; i < days; i++) {
       System.out.println("-------- day " + i + " --------");
       System.out.println("name, sellIn, quality");
